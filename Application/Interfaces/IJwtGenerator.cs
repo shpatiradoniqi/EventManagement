8379bf6a80a901fdac1efe455626ef2c24122d11
Majlindaaaaaaaa <linda.terdevci@gmail.com>
2024-12-22 14:19:55 +0100

    Add files via upload

---
+++ Application/Interfaces/IJwtGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Reactivities.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reactivities.Application.Interfaces
+{
+  public  interface IJwtGenerator
+    {
+        string CreateToken(AppUser user);
+    }
+}
