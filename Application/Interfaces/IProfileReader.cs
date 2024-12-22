a2cfa3fccb7f64871d95a78a09b49a531ae062ba
Majlindaaaaaaaa <linda.terdevci@gmail.com>
2024-12-22 14:21:19 +0100

    Add files via upload

---
+++ Application/Interfaces/IProfileReader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Reactivities.Application.Profiles;
+
+namespace Reactivities.Application.Interfaces
+{
+    public interface IProfileReader
+    {
+        Task<Profile> ReadProfile(string username); 
+    }
+}
