d7d16c1c65c7dae946b2b1f03b972b3b5745054b
Majlindaaaaaaaa <linda.terdevci@gmail.com>
2024-12-22 14:04:46 +0100

    Add files via upload

---
+++ Application/Activities/AttendeeDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reactivities.Application.Activities
+{
+    public class AttendeeDto
+    {
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public string Image { get; set; }
+        public bool IsHost { get; set; }
+        public bool Following { get; set; }
+    }
+}
