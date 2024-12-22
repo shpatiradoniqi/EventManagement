a386812b7e97baf5477c741bd694171859c17dc7
Majlindaaaaaaaa <linda.terdevci@gmail.com>
2024-12-22 13:36:02 +0100

    Add files via upload

---
+++ Domain/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reactivities.Domain
+{
+    public class Comment
+    {
+     public Guid Id { get; set; }
+
+     public string Body { get; set; }
+     
+     public virtual AppUser Author { get; set; }
+
+     public virtual Activity Activity { get; set; }
+     
+     public DateTime CreatedAt { get; set; }
+    }
+}
