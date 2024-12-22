164f24daa34b4c4b8915f6410d89518ec6101b5f
Majlindaaaaaaaa <linda.terdevci@gmail.com>
2024-12-22 13:31:09 +0100

    Add files via upload

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,1 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace Reactivities.Domain
+{
+    public class Activity
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string  Category { get; set; }
+
+        public DateTime Date { get; set; }
+        public string City{ get; set; }
+        public string  Venue { get; set; }
+        public virtual ICollection<UserActivity> UserActivities { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+
+    }
+}
