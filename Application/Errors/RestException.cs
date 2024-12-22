0962462a391c1165cdf502f9479b1c8dbb99c235
Majlindaaaaaaaa <linda.terdevci@gmail.com>
2024-12-22 14:10:36 +0100

    Add files via upload

---
+++ Application/Errors/RestException.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Net;
+
+namespace Reactivities.Application.Errors
+{
+    public class RestException : Exception
+    {
+        public RestException(HttpStatusCode code, object error = null)
+        {
+            Code = code;
+            Errors = error;
+        }
+
+        public HttpStatusCode Code { get; }
+
+        public object Errors { get; }
+    }
+}
