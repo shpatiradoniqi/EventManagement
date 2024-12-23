78ef8746d40668554235e1f3149fea4ef07510ba
coyg2000 <sr47050@ubt-uni.net>
2024-12-23 14:12:06 +0100

    Krijimi i dy kontrollerave dhe klases me metoden Unattend per nje aktivitet te caktuar

---
+++ EventManagement.Server/Controllers/ActivitiesController.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Reactivities.Application.Activities;
+using System.Threading.Tasks;
+using System;
+using EventManagement.Server.Application.Activities;
+
+namespace EventManagement.Server.Controllers
+{
+    public class ActivitiesController : BaseController
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<List.ActivitiesEnvelope>> List(int? limit,
+            int? offset, bool isGoing, bool isHost, DateTime? startDate)
+        {
+            return await Mediator.Send(new List.Query(limit, offset, isGoing, isHost, startDate));
+        }
+
+        [HttpGet("{id}")]
+        //[Authorize]       
+        [AllowAnonymous]
+        public async Task<ActionResult<ActivityDto>> Details(Guid id)
+        {
+            return await Mediator.Send(new Details.Query { Id = id });
+
+        }
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        {
+            return await Mediator.Send(command);
+        }
+        [HttpPut("{id}")]
+        //[Authorize(Policy = "IsActivityHost")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+
+            command.Id = id;
+            return await Mediator.Send(command);
+
+        }
+        [HttpDelete("{id}")]
+        //[Authorize(Policy = "IsActivityHost")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+            return await Mediator.Send(new Delete.Command { Id = id });
+        }
+
+        [HttpPost("{id}/attend")]
+
+        public async Task<ActionResult<Unit>> Attend(Guid id)
+        {
+            return await Mediator.Send(new Attend.Command { Id = id });
+        }
+        [HttpDelete("{id}/attend")]
+
+        public async Task<ActionResult<Unit>> Unattend(Guid id)
+        {
+            return await Mediator.Send(new Unattend.Command { Id = id });
+        }
+    }
+}
