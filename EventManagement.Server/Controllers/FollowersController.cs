988c81db3dd9574adcbe40971850c2c09a9a37f9
coyg2000 <sr47050@ubt-uni.net>
2024-12-23 18:50:26 +0100

    Followers kontroller

---
+++ EventManagement.Server/Controllers/FollowersController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Reactivities.Application.Followers;
+using Reactivities.Application.Profiles;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EventManagement.Server.Controllers
+{
+    [Route("api/profiles")]
+    public class FollowersController : BaseController
+    {
+        [HttpPost("{username}/follow")]
+        public async Task<ActionResult<Unit>> Follow(string username)
+        {
+            return await Mediator.Send(new Add.Command { Username = username });
+        }
+
+        [HttpDelete("{username}/follow")]
+        public async Task<ActionResult<Unit>> Unfollow(string username)
+        {
+            return await Mediator.Send(new Delete.Command { Username = username });
+        }
+
+        [HttpGet("{username}/follow")]
+        public async Task<ActionResult<List<Profile>>> GetFollowers(string username, string predicate)
+        {
+            return await Mediator.Send(new List.Query { Username = username, Predicate = predicate });
+        }
+    }
+}
