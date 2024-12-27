5c928bb6b84bdea8a0067eaa1bf2552d88fefea6
coyg2000 <sr47050@ubt-uni.net>
2024-12-27 20:20:19 +0100

    Kerkesat sipas trellos

--- EventManagement.Server/Controllers/ActivitiesController.cs
+++ EventManagement.Server/Controllers/ActivitiesController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using EventManagement.Server.Application.Activities;
 using System.Threading.Tasks;
 using System;
+using System.Collections.Generic;
 
 namespace EventManagement.Server.Controllers
 {
